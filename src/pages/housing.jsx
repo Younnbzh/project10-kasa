1b288a5ee74ecc2aff3cbc0371497d92c9073516
Younnbzh <trocquer@gmail.com>
2025-03-05 09:42:51 +0100

    Step 7

--- src/pages/housing.jsx
+++ src/pages/housing.jsx
@@ -1,7 +1,15 @@
+import { useParams } from 'react-router-dom';
+import Slideshow from '../components/slideshow';
+import logements from '../data/logements.json';
+
 function Housing() {
+  const { id } = useParams();
+  const logement = logements.find(item => item.id === id);
+  
   return (
     <div className="housing">
-      <h1>Détails du logement</h1>
+      <Slideshow pictures={logement.pictures} />
+      <h1>{logement.title}</h1>
     </div>
   );
 }
